dedef815bc8fd472493b873c0b105fbfeb5f68a1
Ubuntu <ubuntu@ip-172-31-5-27.ec2.internal>
2021-01-08 21:43:36 +0000

    cylinder_dimensions.py

--- cylinder_dimensions.py
+++ cylinder_dimensions.py
@@ -5,52 +5,60 @@
 # This program is a circle area program
 
 import math
-pi = math.pi
 
 
-def surface_area(radius, hight):
+def surface_area(radius, height):
     # this function calculates the surface area of the cylinder
 
     # process
-    surface_area = (2*pi*radius**2)+(2*pi*radius*hight)
-    return
+    surface_area = (2*math.pi*radius**2)+(2*math.pi*radius*height)
 
-def volume(radius, hight):
+    return surface_area
+
+
+def volume(radius, height):
     # this function calculates the volume of the cylinder
 
     # process
-    volume = pi*radius**2*hight
-    return
+    volume = math.pi*radius**2*height
+
+    return volume
 
 
 def perimeter(radius):
     # this function calculates the perimeter of the cylinder
 
     # process
-    perimeter = 2*pi*radius
-    return
+    perimeter = 2*math.pi*radius
+
+    return perimeter
 
 
 def main():
     # input
     while True:
-        user_radius_str = input("Enter the radius: ")
-        user_hight_str = input("Enter the hight: ")
+        user_radius_str = input("Enter the radius (mm): ")
+        user_height_str = input("Enter the height (mm): ")
 
         try:
-            user_radius_str = int(user_radius_str)
-            user_hight_str = int(user_hight_str)
+            user_radius_int = int(user_radius_str)
+            user_height_int = int(user_height_str)
 
             # calling functions
-            surface_area(user_radius_str, user_hight_str)
-            volume(user_radius_str, user_hight_str)
-            perimeter(user_radius_str)
-            print("the surface area is {0}.". format(surface_area))
-            print("the volume of the cylinder {0}.".format(volume))
-            print("the perimeter of the cylinder {0}.".format(perimeter))
+            surface_area_calculated = surface_area(user_radius_int,
+                                                   user_height_int)
+            volume_calculated = volume(user_radius_int,
+                                       user_height_int)
+            perimeter_calculated = perimeter(user_radius_int)
+            print("the surface area is {0:.2f}mm².".
+                  format(surface_area_calculated))
+            print("the volume of the cylinder {0:.2f}mm³.".
+                  format(volume_calculated))
+            print("the perimeter of the cylinder {0:.2f}mm².".
+                  format(perimeter_calculated))
 
             break
-        except:
+        except Exception:
             print("that is invalid input")
             print("try again")
 
